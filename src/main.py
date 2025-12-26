4dfa47ac6c575f2f3c5d9dddb41fe9efdb54e50b
k-pf <k-pf@me>
2025-12-26 03:19:37 +0100

    Update styles to fit the brand colors, add beer style and brewery

--- src/main.py
+++ src/main.py
@@ -11,7 +11,10 @@ BODY_TEMPLATE = Path(PARENT_DIR / "assets/body.html").read_text()
 SNIPPET_TEMPLATE = Path(PARENT_DIR / "assets/snippet.html").read_text()
 SCRIPT_SNIPPET = Path(PARENT_DIR / "assets/script.html").read_text()
 EXPECTED_HEADER = [
+    "tap_num",
+    "brewery",
     "name",
+    "style",
     "abv",
     "image_url",
     "description",
@@ -22,18 +25,24 @@ EXPECTED_HEADER = [
 TAPLIST = Path(PARENT_DIR / "assets/taplist.csv").read_text()
 
 
-def gen_snippet(beer):
-    title = beer["tap_num"] + ". " + beer["name"]
-    image_snippet = (
-        f"<img src='{beer['image_url']}' alt='{beer['name']}' />"
-        if beer["image_url"]
-        else '<div class="placeholder"><i class="fas fa-beer"></i></div>'
-    )
+def format_title(beer):
+    return f"{beer['tap_num']}. {beer['name']}"
 
+
+def format_image(beer):
+    if beer["image_url"]:
+        return f"<img src='{beer['image_url']}' alt='{beer['name']}' />"
+    else:
+        return '<div class="placeholder"><i class="fas fa-beer"></i></div>'
+
+
+def gen_snippet(beer):
     return SNIPPET_TEMPLATE.format(
-        title=title,
+        title=format_title(beer),
+        brewery=beer["brewery"],
+        beerstyle=beer["style"],
         abv=beer["abv"],
-        image_snippet=image_snippet,
+        image_snippet=format_image(beer),
         description=beer["description"],
         price_big=beer["price_big"],
         price_small=beer["price_small"],
@@ -49,15 +58,7 @@ def get_snippets(csv_reader):
 
 if __name__ == "__main__":
     reader = csv.DictReader(io.StringIO(TAPLIST))
-    if reader.fieldnames != [
-        "tap_num",
-        "name",
-        "abv",
-        "image_url",
-        "description",
-        "price_big",
-        "price_small",
-    ]:
+    if reader.fieldnames != EXPECTED_HEADER:
         raise ValueError(
             f"Unexpected CSV header, expected: {EXPECTED_HEADER}, got: {reader.fieldnames}"
         )
